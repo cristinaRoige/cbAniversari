c6308c54c6e75efca64c10237fb1984a07c57d47
cristinaRoige <cristinaroige75@gmail.com>
2018-09-23 05:02:08 +0200

    primer commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*
+$(document).ready(function(){
+	$('#obrirModal').click(obrirFinestra);
+	$('#tancarModal').click(tancarFinestra);
+});
+
+function obrirFinestra(){
+	$(".finestra").slideDown("slow");
+}
+
+function tancarFinestra(){
+	$(".finestra").slideUp("fast");
+}
+
+$(document).ready(function(){
+      $("a.scroll").scrollomatic();
+    });
+*/
+
+$(document).ready(function(){
+	$("a.scroll").scrollomatic();
+});
